97e7d8856eeed957750c998947ed6a3c98173824
matsilva <matsilva@frba.utn.edu.ar>
2026-05-01 23:13:02 -0300

    wip: experimento roto

--- operaciones.c
+++ operaciones.c
@@ -22,6 +22,6 @@ int multiplicar(int a, int b) {
 /* ── esPar ────────────────────────────────────────────────────────────────── */
 
 int esPar(int n) {
-    return (n % 2) == 0;
+    return (n % 2) == 0;// esperimento roto
 
 }
